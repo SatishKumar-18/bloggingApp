f2991d97667539f3656e62c9fa95492d8f8f2e7f
Satish Kumar Kanhar <satishkanhar.18@gmail.com>
2024-09-14 21:33:50 +0530

    initial project setup

---
+++ src/test/java/com/project/BloggingApp/BloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BloggingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
